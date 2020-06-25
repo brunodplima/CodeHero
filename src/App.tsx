eef2e118eb958a18898da416d9e0be54b6978696
Bruno Lima <brunodplima@gmail.com>
2020-06-25 20:46:33 -0300

    Configurar estilo de código

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <h1>Code Hero</h1>
-  );
+  )
 }
 
-export default App;
+export default App
